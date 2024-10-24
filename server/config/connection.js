30a9b2c8a7087f13e195d41e004d410e83ccebbf
melanieprimpas <melanie.primpas@gmail.com>
2024-10-24 19:24:02 -0400

    fixed the bugs, now the server works

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,16 @@
 // Enable access to .env variables
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '../.env', debug: true });
+console.log(import.meta.url)
 
 import { Sequelize } from 'sequelize';
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD)
+//console.log(import.meta.env.DB_NAME, import.meta.env.DB_USER, import.meta.env.DB_PASSWORD)
 
 // Create a connection object
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
       dialectOptions: {
