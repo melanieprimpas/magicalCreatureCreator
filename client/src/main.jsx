43268035bdb240b82f9a0f6d1dfac13625ad69a1
melanieprimpas <melanie.primpas@gmail.com>
2024-10-29 03:47:33 -0400

    merged auth code with rest of coode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { AuthProvider } from './context/AuthContext'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider><App /></AuthProvider>
-    
-  </React.StrictMode>
-)
+import App from './App.jsx'
+import './index.css'
+
+import BrowseCreatures from './pages/BrowseCreatures.jsx';
+import CreateCreatures from './pages/CreateCreatures.jsx';
+import SavedCreatures from './pages/SavedCreatures.jsx'
+import ErrorPage from './pages/ErrorPage.jsx';
+import Login from './components/Login.jsx';
+import { Dashboard } from './components/Dashboard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <BrowseCreatures />,
+      },
+      {
+        path: '/SavedCreatures',
+        element: <SavedCreatures />,
+      },
+      {
+        path: '/CreateCreatures',
+        element: <CreateCreatures />,
+      },
+      {
+        path: '/login',
+        element: <Login/>,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
